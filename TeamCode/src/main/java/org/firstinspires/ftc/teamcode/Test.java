8ffc6ec63600072745cc7ac0ed0f2554cadfa459
Gabriel Reuter <117861576+Gabeyhi@users.noreply.github.com>
2022-11-15 18:40:21 -0800

    Syntax fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-@Teleop(name="TestBalls", group="TeleOp")
+@TeleOp(name="TestBalls", group="TeleOp")
 public class Test extends LinearOpMode {
 
 
