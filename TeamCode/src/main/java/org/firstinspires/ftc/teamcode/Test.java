54a4b38381e026cf7bbac91a62753f690931c928
Gabeyhi <117861576+Gabeyhi@users.noreply.github.com>
2022-11-15 18:34:58 -0800

    Added Example

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+@Teleop(name="TestBalls", group="TeleOp")
+public class Test extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor motorFrontLeft  = hardwareMap.dcMotor.get("LeftBall");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("RightBall");
+        DcMotor motorBackLeft   = hardwareMap.dcMotor.get("BackLeftBall");
+        DcMotor motorBackRight  = hardwareMap. dcMotor. get("BackRightBall");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double ly = -gamepad1.left_stick_y; 
+            double lx = gamepad1.left_stick_x; 
+            double rx = gamepad1.right_stick_x;
+            double denominator = Math.max(Math.abs(ly) + Math.abs(lx) + Math.abs(lx), 1);
+
+            motorFrontLeft.setPower((ly + lx + rx) / denominator); 
+            motorFrontRight.setPower((ly - lx - rx) / denominator);
+            motorBackLeft.setPower((ly - lx + rx) / denominator);
+            motorBackRight.setPower((ly + lx - rx) / denominator);
+
+            idle();
+        }
+
+    }
+}
